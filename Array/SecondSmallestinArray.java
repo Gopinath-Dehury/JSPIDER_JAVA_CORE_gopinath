d699c8ead258eb55eae2859883d9605c195cd46e
Gopinath Dehury <gopinath90biku@gmail.com>
2025-05-05 00:54:05 +0530

    yello

--- Array/SecondSmallestinArray.java
+++ Array/SecondSmallestinArray.java
@@ -1,21 +1,15 @@
 public class SecondSmallestinArray {
     public static void main(String[] args) {
-        int[] a = { 3, 8, 4, 3, 5, 7, 9, 2, 12 };
-        int s1, s2;
+        int[] a = { 3, 8, 4, 3, 5, 7, 9, 12 };
+        int s1=a[0];
+        int s2=a[0];
 
-        if(a[0]<a[1]){
-             s1 = a[0];
-             s2 = a[1];
-        }else{
-             s1 = a[1];
-             s2 = a[0];
-        }
-
-        for (int i = 2; i < a.length; i++) {
-            if (s1 > a[i]) {
+        for (int i = 1; i < a.length; i++) {
+            if (s1 == a[i]) continue;
+            if(a[i]<s1) {
                 s2 = s1;
                 s1 = a[i];
-            }else if(s2>a[i] && s1!=a[i]){
+            }else if(s2>a[i] && s1==s2){
                 s2=a[i];
             }
         }
